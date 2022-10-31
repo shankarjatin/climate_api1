5c5a852a64cb263edf3a0b4dcc5f69c6bf1f60a1
Jatin shankar <shankarjatin1005@gmail.com>
2022-10-31 22:02:31 +0530

    changes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const https = require("https");
@@ -7,8 +8,11 @@ const url = "https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=75ce
 https.get(url ,function(response){
     console.log(response);
 })
-   
+response.on("data", function(data){
+const weatherData = JSON.parse(data)
+console.log(weatherData);
+ })
 })
 
 
-app.listen(7000, function(){console.log("server is up on port 7000")})
+app.listen(7000, function(){console.log("server is up on port 7000")});
