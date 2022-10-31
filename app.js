206c0f3b4703d0852c6b7c4412be58b8069f85bb
Jatin shankar <shankarjatin1005@gmail.com>
2022-10-31 19:44:02 +0530

    https get method added through node

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
+const https = require("https");
+
+app.get("/",function(req,res){
+const url = "https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=75ce89184100fe8aca406193679279fe";
+https.get(url ,function(response){
+    console.log(response);
+})
+   
+})
 
 
-
-app.listen(7000, function(){console.log("server is up on port 7000")});
+app.listen(7000, function(){console.log("server is up on port 7000")})
