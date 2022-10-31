ebf2208a6d839e7a41ce9659849310ff5d88775c
Jatin shankar <shankarjatin1005@gmail.com>
2022-10-31 22:36:19 +0530

    json parse added

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const { response } = require("express");
 const express = require("express");
 const app = express();
 const https = require("https");
 
-app.get("/",function(req,res){
+app.get("/",function(req,res){})
 const url = "https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=75ce89184100fe8aca406193679279fe";
-https.get(url ,function(response){
-    console.log(response);
-})
-response.on("data", function(data){
+
+https.get(url ,function(res){
+console.log(res.statusCode);
+
+res.on("data", function(data){
 const weatherData = JSON.parse(data)
 console.log(weatherData);
- })
+})
 })
 
-
-app.listen(7000, function(){console.log("server is up on port 7000")});
+app.listen(7000, function(){
+    console.log("server is up on port 7000")
+})
