34b741a1e5ad7a57c36a9bba5388189dec30216c
Jatin shankar <shankarjatin1005@gmail.com>
2022-11-01 23:56:57 +0530

    comment added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const https = require("https");
 
 app.get("/",function(req,res){})
 const url = "https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=75ce89184100fe8aca406193679279fe";
-
+// to get status
 https.get(url ,function(res){
 console.log(res.statusCode);
 
